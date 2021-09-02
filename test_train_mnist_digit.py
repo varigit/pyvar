c1dcde1640d4dc8a0937e4e206d013c302fddf83
Diego Dorta <diego.d@variscite.com>
2021-09-02 20:27:31 -0300

    Split train and test
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- test_train_mnist_digit.py
+++ test_train_mnist_digit.py
@@ -2,9 +2,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from pyvarml.dataset.mnist import train_mnist_digit
-from pyvarml.engines.tflite import run_inference
-from pyvarml.utils.convert_images import convert_rgb_to_gray_scale
-from pyvarml.utils.convert_images import get_result
 
 trained_model = train_mnist_digit()
 
@@ -14,12 +11,3 @@ with open('mnist_digit.tflite', "wb") as model_file:
 
 print(f"Test loss: {trained_model[1]}")
 print(f"Test accuracy: {trained_model[2]}")
-
-input_image = convert_rgb_to_gray_scale("zero.png", 28, 28)
-
-result = run_inference("mnist_digit.tflite", input_image)
-
-predict_digit = get_result(result)
-
-print(f"Predicted Digit: {predict_digit}")
-print(f"Confidence: {result[predict_digit]}")
--- test_train_mnist_fashion.py
+++ test_train_mnist_fashion.py
@@ -2,9 +2,6 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from pyvarml.dataset.mnist import train_mnist_fashion
-from pyvarml.engines.tflite import run_inference
-from pyvarml.utils.convert_images import convert_rgb_to_gray_scale
-from pyvarml.utils.convert_images import get_result
 
 trained_model = train_mnist_fashion()
 
@@ -14,12 +11,3 @@ with open('mnist_fashion.tflite', "wb") as model_file:
 
 print(f"Test loss: {trained_model[1]}")
 print(f"Test accuracy: {trained_model[2]}")
-
-input_image = convert_rgb_to_gray_scale("shirt.png", 28, 28)
-
-result = run_inference("mnist_fashion.tflite", input_image)
-
-predict_flashion = get_result(result)
-
-print(f"Predicted Fashion: {predict_flashion}")
-print(f"Confidence: {result[predict_flashion]}")
