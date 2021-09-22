f5b8d0bb13e6f8a81c0b5338c079c70d93308652
Diego Dorta <diego.d@variscite.com>
2021-09-21 23:57:13 -0300

    Add comments and condition to expand dimensions
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- pyvarml/utils/helper.py
+++ pyvarml/utils/helper.py
@@ -1,6 +1,14 @@
 # Copyright 2021 Variscite LTD
 # SPDX-License-Identifier: BSD-3-Clause
 
+"""
+:platform: Unix/Yocto
+:synopsis: Python Class to  manipulate images/frames.
+
+.. moduleauthor:: Diego Dorta <diego.d@variscite.com>
+    .. note:: 10/15/2021 [diego.d] First Version Released
+"""
+
 import sys
 
 import cv2
@@ -14,7 +22,17 @@ class Image:
             self.image = image_file    
         self.converted = None     
 
-    def convert_rgb_to_gray_scale(self, width = None, height = None):
+    def convert_rgb_to_gray_scale(self, width=None, height=None, expand_dims=True):
+        """
+        Convert RGB images/frames to gray scale.
+
+        Args:
+            width (int): image width
+            height (int): image height
+            expand_dims (bool): expand dimensions
+
+        The gray scale image/frame is storage in the **converted** attribute.
+        """
         if width is None:
             sys.exit("Please specify width!")
         if height is None:
@@ -22,4 +40,5 @@ class Image:
         img = cv2.imread(self.image)
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         img = cv2.resize(img, (width, height), interpolation = cv2.INTER_LINEAR)
-        self.converted = np.expand_dims(np.array(img, dtype=np.float32) / 255.0, 0)
+        self.converted = np.expand_dims(np.array(
+                            img, dtype=np.float32) / 255.0, 0) if expand_dims else img
