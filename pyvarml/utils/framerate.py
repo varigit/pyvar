261665395282a7e966cde1388276367f9eb9252b
Diego Dorta <diego.d@variscite.com>
2021-09-21 23:55:53 -0300

    Add comments
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- pyvarml/utils/framerate.py
+++ pyvarml/utils/framerate.py
@@ -1,6 +1,14 @@
 # Copyright 2021 Variscite LTD
 # SPDX-License-Identifier: BSD-3-Clause
 
+"""
+:platform: Unix/Yocto
+:synopsis: Python Class to get framerate from videos and real time cameras.
+
+.. moduleauthor:: Diego Dorta <diego.d@variscite.com>
+    .. note:: 10/15/2021 [diego.d] First Version Released
+"""
+
 import collections
 from contextlib import contextmanager
 from time import monotonic
@@ -12,6 +20,9 @@ class Framerate:
 
     @contextmanager
     def fpsit(self):
+        """
+        The framerate is storage in the **fps** attribute.
+        """
         begin = monotonic()
         try:
             yield
