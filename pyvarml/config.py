22a9765f15d3db2dceaf3ff3134d472f303f2696
Diego Dorta <diego.d@variscite.com>
2021-09-21 23:58:27 -0300

    Add config file
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ pyvarml/config.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Variscite LTD
+# SPDX-License-Identifier: BSD-3-Clause
+
+import os
+
+# FTP Credentials
+FTP_HOST = "ftp.variscite.com"
+FTP_USER = "customerv"
+FTP_PASS = "Variscite1"
+
+# Local Files
+CACHEDIR = os.path.join(os.environ['HOME'], ".cache", "pyvarml")
+
+# Extensions
+TFLITE = "*.tflite"
+TXT = "*.txt"
+ZIP = ".zip"
