d2c7271d9f7cb9e5105d01eaacf0bfd1ef317722
Diego Dorta <diego.d@variscite.com>
2021-09-26 01:20:31 -0300

    Add sphinx
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ sphinx/conf.py
@@ -0,0 +1,32 @@
+import sphinx_press_theme
+import os
+import sys
+sys.path.insert(0, os.path.abspath('..'))
+
+project = 'Variscite LTD'
+copyright = '2021, Variscite LTD'
+author = 'Variscite LTD'
+
+release = '0.0.1'
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'press'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
---
+++ sphinx/index.rst
@@ -0,0 +1,28 @@
+Variscite Python API for Machine Learning
+=========================================
+
+Pyvarml is a Python package that provides a set of high-level abstraction to
+allow any user to develop their own Machine Learning application in a simplified
+and efficiently way without writing too many lines of code.
+
+Contents
+---------
+
+.. toctree::
+   :maxdepth: 2
+
+   pyvarml.overview.rst
+   pyvarml.gettingstarted.rst
+   pyvarml.engines.rst
+   pyvarml.multimedia.rst
+   pyvarml.dataset.rst
+   pyvarml.utils.rst
+   pyvarml.engines.examples.rst
+   pyvarml.dataset.examples.rst
+   pyvarml.utils.examples.rst
+   pyvarml.classification.examples.rst
+   pyvarml.detection.examples.rst
+   pyvarml.license.rst
+   pyvarml.contribute.rst
+
+
