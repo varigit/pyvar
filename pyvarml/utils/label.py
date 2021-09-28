769fb106bed5fe096675c9823dad54b10fe2341a
Diego Dorta <diego.d@variscite.com>
2021-09-27 21:47:18 -0300

    Add comments
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- pyvarml/utils/label.py
+++ pyvarml/utils/label.py
@@ -11,30 +11,44 @@
 
 import re
 
+CLASSIFICATION = "classification"
+DETECTION = "detection"
+
 class Label:
     """
-    Python Class to read the labels file.
+    Python Class to read the labels file from Machine Learning models.
 
-    :ivar labels_file: variable to storage the labels file path;
-    :ivar list: variable to storage the read labels;
+    :ivar labels_file: storages the path of the labels model file.
+    :ivar list: storages the read labels from the model file.
     """
-    def __init__(self, labels_file_path, category=None):
+    def __init__(self, labels_file_path=None):
         self.labels_file = labels_file_path
-        self.category = category
         self.list = []
-        self.read_labels()
 
-    def read_labels(self):
+    def read_labels(self, category=None):
         """
-        Method to read the labels file and save it as a list.
+        Method to read the labels file and save the result in list attribute.
+
+        Args:
+            category (str): storages the model category.
+
+        Note:
+             **category**: choose between classification or detection.
+
+        Returns:
+            If **success** returns **True**, if **not** returns **False**.
         """
-        if self.category is "classification":
-            with open(self.labels_file, 'r') as f:
-                self.list = [line.strip() for line in f.readlines()]
-                f.close()
-        elif self.category is "detection":
-            p = re.compile(r'\s*(\d+)(.+)')
+        if self.labels_file is None:
+            return False
+        if category is None:
+            return False
+        else:
             with open(self.labels_file, 'r', encoding='utf-8') as f:
-                lines = (p.match(line).groups() for line in f.readlines())
-                self.list =  {int(num): text.strip() for num, text in lines}
+                if category is CLASSIFICATION:
+                    self.list = [line.strip() for line in f.readlines()]
+                elif category is DETECTION:
+                    p = re.compile(r'\s*(\d+)(.+)')
+                    lines = (p.match(line).groups() for line in f.readlines())
+                    self.list =  {int(num): text.strip() for num, text in lines}
                 f.close()
+        return True
