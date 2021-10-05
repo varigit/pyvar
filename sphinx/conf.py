d044b6b27ecb72791d62d5b64eeb3232127df8c7
Diego Dorta <diego.d@variscite.com>
2021-10-05 19:13:29 -0300

    Add viewcode extension
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- sphinx/conf.py
+++ sphinx/conf.py
@@ -4,12 +4,12 @@ import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 project = 'Variscite LTD'
-copyright = '2021, Variscite LTD'
+copyright = '2021 Variscite LTD'
 author = 'Variscite LTD'
 
 release = '0.0.1'
 
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'sphinx.ext.viewcode']
 
 templates_path = ['_templates']
 
@@ -30,5 +30,7 @@ html_theme = 'press'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+html_show_sourcelink = True
 
 html_logo = 'images/logo.png'
