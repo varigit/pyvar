0be19fa5de472a152b32ac9407b3fc7296c2ef5b
Diego Dorta <diego.d@variscite.com>
2021-10-05 23:16:33 -0300

    Add semantic segmentation docs
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ sphinx/examples/segmentation/image_segmentation.py
@@ -0,0 +1,28 @@
+from pyvarml.engines.tflite import TFLiteInterpreter
+from pyvarml.multimedia.images import Images
+from pyvarml.utils.overlay import Overlay
+from pyvarml.utils.retriever import FTP
+
+ftp = FTP()
+
+if ftp.retrieve_package(category="segmentation"):
+    model_file_path = ftp.model
+
+engine = TFLiteInterpreter(model_file_path)
+
+image_test = Images("path/to/image") # Change here
+image_test.resize(engine_input_details=engine.input_details)
+
+engine.set_input(image_test.resized)
+engine.run_inference()
+engine.get_segmentation_result(image=image_test.resized_no_dims)
+
+draw = Overlay()
+draw.scores_info = False
+
+output_image = draw.info(
+                    "classification", engine.output_image,
+                    None, None, engine.inference_time,
+                    model_file_path, image_test.image)
+
+image_test.show("Image Semantic Segmentation Example", output_image)
