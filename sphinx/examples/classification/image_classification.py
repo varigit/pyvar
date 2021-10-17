21571d2256909671661152c272b7858ca7766704
Diego Dorta <diego.d@variscite.com>
2021-10-16 21:48:19 -0300

    Change examples to new multimedia class
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- sphinx/examples/classification/image_classification.py
+++ sphinx/examples/classification/image_classification.py
@@ -1,8 +1,12 @@
+# Copyright 2021 Variscite LTD
+# SPDX-License-Identifier: BSD-3-Clause
+
 from pyvarml.engines.tflite import TFLiteInterpreter
-from pyvarml.multimedia.images import Images
+from pyvarml.multimedia.helper import Multimedia
 from pyvarml.utils.label import Label
 from pyvarml.utils.overlay import Overlay
 from pyvarml.utils.retriever import FTP
+from pyvarml.utils.resizer import Resizer
 
 ftp = FTP()
 
@@ -15,18 +19,24 @@ labels.read_labels("classification")
 
 engine = TFLiteInterpreter(model_file_path)
 
-image_test = Images("path/to/image") # Change here
-image_test.resize(engine_input_details=engine.input_details)
+resizer = Resizer()
+resizer.set_sizes(engine_input_details=engine.input_details)
 
-engine.set_input(image_test.resized)
+image = Multimedia("media/car.jpg")
+resizer.resize_image(image.video_src)
+
+engine.set_input(resizer.image_resized)
 engine.run_inference()
 engine.get_result("classification")
 
 draw = Overlay()
 
-output_image = draw.info(
-                    "classification", image_test.image_original,
-                    engine.result, labels.list, engine.inference_time,
-                    model_file_path, image_test.image)
+output_image = draw.info(category="classification",
+                         image=resizer.image,
+                         top_result=engine.result,
+                         labels=labels.list,
+                         inference_time=engine.inference_time,
+                         model_name=model_file_path,
+                         source_file=resizer.image_path)
 
-image_test.show("Image Classification Example", output_image)
+image.show_image("Image Classification Example", output_image)
--- sphinx/examples/classification/realtime_classification.py
+++ sphinx/examples/classification/realtime_classification.py
@@ -1,9 +1,13 @@
+# Copyright 2021 Variscite LTD
+# SPDX-License-Identifier: BSD-3-Clause
+
 from pyvarml.engines.tflite import TFLiteInterpreter
-from pyvarml.multimedia.realtime import RealTime
+from pyvarml.multimedia.helper import Multimedia
 from pyvarml.utils.framerate import Framerate
 from pyvarml.utils.label import Label
 from pyvarml.utils.overlay import Overlay
 from pyvarml.utils.retriever import FTP
+from pyvarml.utils.resizer import Resizer
 
 ftp = FTP()
 
@@ -16,9 +20,11 @@ labels.read_labels("classification")
 
 engine = TFLiteInterpreter(model_file_path)
 
-camera = RealTime("path/to/device") # Change here
-camera.start()
-camera.set_sizes(engine_input_details=engine.input_details)
+resizer = Resizer()
+resizer.set_sizes(engine_input_details=engine.input_details)
+
+camera = Multimedia("/dev/video1", resolution="vga")
+camera.set_v4l2_config()
 
 framerate = Framerate()
 
@@ -28,17 +34,21 @@ draw.framerate_info = True
 while camera.loop:
     with framerate.fpsit():
         frame = camera.get_frame()
-        camera.resize_frame(frame)
+        resizer.resize_frame(frame)
 
-        engine.set_input(camera.frame_resized)
+        engine.set_input(resizer.frame_resized)
         engine.run_inference()
         engine.get_result("classification")
 
-        output_frame = draw.info(
-                            "classification", camera.frame_original,
-                            engine.result, labels.list, engine.inference_time,
-                            model_file_path, camera.camera, framerate.fps)
+        output_frame = draw.info(category="classification",
+                                 image=resizer.frame,
+                                 top_result=engine.result,
+                                 labels=labels.list,
+                                 inference_time=engine.inference_time,
+                                 model_name=model_file_path,
+                                 source_file=camera.dev.name,
+                                 fps=framerate.fps)
 
-        camera.show_frame("Real Time Classification Example", output_frame)
+        camera.show("Real Time Classification Example", output_frame)
 
 camera.destroy()
