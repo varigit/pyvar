5a351957bcdefd542651864ff41e8908db37b7ab
Diego Dorta <diego.d@variscite.com>
2021-11-15 15:12:11 -0300

    Add sphinx comments
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- pyvar/ml/utils/resizer.py
+++ pyvar/ml/utils/resizer.py
@@ -6,7 +6,6 @@
 :synopsis: Python Resizer Class
 
 .. moduleauthor:: Diego Dorta <diego.d@variscite.com>
-    .. note:: 10/15/2021 [diego.d] First Version Released
 """
 
 import cv2
@@ -14,6 +13,17 @@ import numpy as np
 from PIL import Image
 
 class Resizer:
+    """
+    Python Class to resize images and frames.
+
+    :ivar frame: storages the original frame;
+    :ivar frame_resized: storages the resized frame;
+    :ivar image: storages the original image;
+    :ivar image_path: storages the image path;
+    :ivar image_resized: storages the image resized;
+    :ivar model_height: storages the model height;
+    :ivar model_width: storages the model width.
+    """
     def __init__(self):
         self.frame = None
         self.frame_resized = None
@@ -24,10 +34,25 @@ class Resizer:
         self.model_width = None
 
     def set_sizes(self, engine_input_details=None):
+        """
+        Set the height and width model size.
+        """
         if engine_input_details is not None:
             _, self.model_height, self.model_width, _ = engine_input_details[0]['shape']
    
     def resize_frame(self, frame=None, width=None, height=None, expand_dims=True):
+        """
+        Resize the frame.
+
+        Args:
+            frame (numpy array): original frame to be resized;
+            width (int): new width to resize;
+            height (int): new height to resize;
+            expand_dims (bool): if expand dimensions or not
+
+        Returns:
+            The image (numpy array) resized is storaged at the frame_resized attribute.
+        """
         if frame is not None:
             self.frame = frame
             if width is not None and isinstance(width, int):
@@ -39,6 +64,18 @@ class Resizer:
                 self.frame_resized = np.expand_dims(self.frame_resized, axis=0)
 
     def resize_image(self, image_path=None, width=None, height=None, expand_dims=True):
+        """
+        Resize the image.
+
+        Args:
+            image_path (numpy array): original image to be resized;
+            width (int): new width to resize;
+            height (int): new height to resize;
+            expand_dims (bool): if expand dimensions or not
+
+        Returns:
+            The image (numpy array) resized is storaged at the image_resized attribute.
+        """
         if image_path is not None:
             self.image_path = image_path
             if width is not None and isinstance(width, int):
