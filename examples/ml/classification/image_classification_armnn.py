7f667a9b9e24bff6740e6c5aa0d178c3347e2529
Diego Dorta <diego.d@variscite.com>
2021-12-07 13:07:03 -0300

    Add image classification using armnn inference engine
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

---
+++ examples/ml/classification/image_classification_armnn.py
@@ -0,0 +1,33 @@
+# Copyright 2021 Variscite LTD
+# SPDX-License-Identifier: BSD-3-Clause
+
+from pyvar.ml.engines.armnn import ArmNNInterpreter
+from pyvar.ml.utils.label import Label
+from pyvar.ml.utils.retriever import FTP
+from pyvar.ml.utils.resizer import Resizer
+from pyvar.multimedia.helper import Multimedia
+
+ftp = FTP()
+
+if ftp.retrieve_package(category="classification"):
+    model_file_path = ftp.model
+    label_file_path = ftp.label
+    image_file_path = ftp.image
+
+labels = Label(label_file_path)
+labels.read_labels("classification")
+
+engine = ArmNNInterpreter(model_file_path)
+
+resizer = Resizer()
+
+image = Multimedia(image_file_path)
+resizer.resize_armnn(image.video_src, engine.input_width, engine.input_height)
+
+engine.set_input(resizer.image_resized)
+
+engine.run_inference()
+
+engine.get_result("classification", labels.list)
+
+print(f"Inference time: {engine.inference_time}")
