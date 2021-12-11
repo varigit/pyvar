3bc4e6638dcd4fe4b0bcd2959241cbb4b366d3fd
Diego Dorta <diego.d@variscite.com>
2021-12-11 17:51:55 -0300

    Refactor engines/armnn following PEP 8 guidelines
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- pyvar/ml/engines/armnn.py
+++ pyvar/ml/engines/armnn.py
@@ -22,6 +22,7 @@ from pyvar.ml.config import CLASSIFICATION
 from pyvar.ml.config import DETECTION
 from pyvar.ml.utils.timer import Timer
 
+
 class ArmNNInterpreter:
     """
     :ivar interpreter: Arm NN interpreter;
@@ -32,10 +33,10 @@ class ArmNNInterpreter:
     :ivar model_file_path: path to the machine learning model;
     :ivar input_width: size of model input width;
     :ivar input_height: size of model input height;
-    :ivar category: model category (classification or detection);
     :ivar accelerated: runs inference on NPU or CPU.
     """
-    def __init__(self, model_file_path=None, accelerated=True, category=None):
+
+    def __init__(self, model_file_path=None, accelerated=True):
         """
         Constructor method for the Arm NN class.
         """
@@ -45,10 +46,10 @@ class ArmNNInterpreter:
             raise TypeError(f"Expects {model_file_path} to be a text file")
         self.model_file_path = model_file_path
         self.accelerated = accelerated
-        self.category = category
         self.interpreter = None
         self.input_details = None
         self.output_details = None
+        self.input_tensors = None
         self.inference_time = None
         self.input_width = None
         self.input_height = None
@@ -62,23 +63,23 @@ class ArmNNInterpreter:
         input_names = parser.GetSubgraphInputTensorNames(graph_id)
 
         self.input_binding_info = parser.GetNetworkInputBindingInfo(
-                                                   graph_id, input_names[0])
+            graph_id, input_names[0])
         self.input_width = self.input_binding_info[1].GetShape()[1]
         self.input_height = self.input_binding_info[1].GetShape()[2]
 
         options = ann.CreationOptions()
         self.runtime = ann.IRuntime(options)
 
-        preferredBackends = [ann.BackendId('CpuAcc'),
-                             ann.BackendId('CpuRef')]
+        preferred_backends = [ann.BackendId('CpuAcc'),
+                              ann.BackendId('CpuRef')]
 
         if accelerated is True:
-            preferredBackends = [ann.BackendId('VsiNpu'),
-                                 ann.BackendId('CpuAcc'),
-                                 ann.BackendId('CpuRef')]
+            preferred_backends = [ann.BackendId('VsiNpu'),
+                                  ann.BackendId('CpuAcc'),
+                                  ann.BackendId('CpuRef')]
 
         opt_network, messages = ann.Optimize(
-            network, preferredBackends,
+            network, preferred_backends,
             self.runtime.GetDeviceSpec(), ann.OptimizerOptions())
 
         net_id, _ = self.runtime.LoadNetwork(opt_network)
@@ -86,26 +87,21 @@ class ArmNNInterpreter:
         output_names = parser.GetSubgraphOutputTensorNames(graph_id)
 
         self.output_binding_info = parser.GetNetworkOutputBindingInfo(
-                                                    0, output_names[0])
-        if self.category is not None:
-            if self.category is CLASSIFICATION:
-                self.output_tensors = ann.make_output_tensors(
-                                               [self.output_binding_info])
-            elif self.category is DETECTION:
-                 output_list = []
-                 for out in output_names:
-                     output_list.append(
-                            parser.GetNetworkOutputBindingInfo(graph_id, out))
-                     self.output_tensors = ann.make_output_tensors(output_list)
+            0, output_names[0])
+        output_list = []
+        for out in output_names:
+            output_list.append(
+                parser.GetNetworkOutputBindingInfo(graph_id, out))
+        self.output_tensors = ann.make_output_tensors(output_list)
 
     def set_input(self, image):
         """
-        Set the image/frame into the input tensor to be inferenced.
+        Set the image/frame into the input tensor to be inferred.
         """
         self.input_tensors = ann.make_input_tensors(
-                                      [self.input_binding_info], [image])
+            [self.input_binding_info], [image])
 
-    def get_output(self, index, squeeze=False):
+    def __get_output(self, index, squeeze=False):
         """
         Get the result after running the inference.
 
@@ -119,8 +115,8 @@ class ArmNNInterpreter:
         """
         if squeeze:
             return np.squeeze(
-                      ann.workload_tensors_to_ndarray(
-                                   self.output_tensors)[index][0])
+                ann.workload_tensors_to_ndarray(
+                    self.output_tensors)[index][0])
         return ann.workload_tensors_to_ndarray(self.output_tensors)[index][0]
 
     def get_result(self, category=None):
@@ -134,19 +130,19 @@ class ArmNNInterpreter:
             if **success**, return **True**
             if **not**, return **False**
         """
-        if self.category is not None:
-            if self.category is CLASSIFICATION:
-                output = self.get_output(0)
+        if category is not None:
+            if category is CLASSIFICATION:
+                output = self.__get_output(0)
                 top_k = np.argsort(output)[-5:][::-1]
                 self.result = []
                 for i in top_k:
                     score = float(output[i] / 255.0)
                     self.result.append((i, score))
                 return True
-            elif self.category is DETECTION:
-                positions = self.get_output(0, True)
-                classes = self.get_output(1, True)
-                scores = self.get_output(2, True)
+            elif category is DETECTION:
+                positions = self.__get_output(0, True)
+                classes = self.__get_output(1, True)
+                scores = self.__get_output(2, True)
                 self.result = []
                 for idx, score in enumerate(scores):
                     if score > 0.5:
@@ -160,7 +156,7 @@ class ArmNNInterpreter:
         """
         timer = Timer()
         self.runtime.EnqueueWorkload(
-            0, self.input_tensors, self.output_tensors) # ignores the warm-up time.
+            0, self.input_tensors, self.output_tensors)  # ignores the warm-up time.
         with timer.timeit():
             self.runtime.EnqueueWorkload(0, self.input_tensors, self.output_tensors)
         self.inference_time = timer.time
