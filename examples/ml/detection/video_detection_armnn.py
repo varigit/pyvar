25dc1d98ed6521876b0ec20d084700492e8671c2
Diego Dorta <diego.d@variscite.com>
2021-12-11 17:53:07 -0300

    Refactor examples/video_detection_armnn following PEP 8 guidelines
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- examples/ml/detection/video_detection_armnn.py
+++ examples/ml/detection/video_detection_armnn.py
@@ -2,11 +2,10 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 from pyvar.ml.engines.armnn import ArmNNInterpreter
-from pyvar.ml.utils.framerate import Framerate
 from pyvar.ml.utils.label import Label
 from pyvar.ml.utils.overlay import Overlay
-from pyvar.ml.utils.retriever import FTP
 from pyvar.ml.utils.resizer import Resizer
+from pyvar.ml.utils.retriever import FTP
 from pyvar.multimedia.helper import Multimedia
 
 ftp = FTP()
@@ -19,7 +18,7 @@ if ftp.retrieve_package(category="detection"):
 labels = Label(label_file_path)
 labels.read_labels("detection")
 
-engine = ArmNNInterpreter(model_file_path, accelerated=True, category="detection")
+engine = ArmNNInterpreter(model_file_path, accelerated=True)
 
 resizer = Resizer()
 
