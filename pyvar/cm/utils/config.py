7d0d936c3ad14b3297dac4467083315df5c36083
Alifer Moraes <alifer.m@variscite.com>
2022-02-07 00:13:57 -0300

    Cortex-M: Add config module
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

---
+++ pyvar/cm/utils/config.py
@@ -0,0 +1,14 @@
+# Copyright 2022 Variscite LTD
+# SPDX-License-Identifier: BSD-3-Clause
+
+CMFIRMWARE_DIR = "/lib/firmware"
+
+CM_CONF = "/etc/remoteproc/variscite-rproc.conf"
+REMOTEPROC_DIR = "/sys/class/remoteproc"
+
+FIRMWARE = "/sys/class/remoteproc/remoteproc0/firmware"
+STATE = "/sys/class/remoteproc/remoteproc0/state"
+
+MACHINE = "/sys/devices/soc0/machine"
+
+TTY = "/dev/ttyRPMSG30"
