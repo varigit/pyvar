5e96aef646e1cd8ee23b94294acc8e9db60f47d1
Alifer Moraes <alifer.m@variscite.com>
2022-02-20 22:38:44 -0300

    Add documentation for the cm.core module
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- pyvar/cm/core.py
+++ pyvar/cm/core.py
@@ -17,7 +17,15 @@ from .utils.helper import *
 
 
 class CortexM:
+    """
+    :ivar module: SoM type;
+    :ivar core: Cortex-M core in use;
+    """
+
     def __init__(self, core_n=0):
+        """
+        Constructor method for the Cortex-M class.
+        """
         self.module = get_module()
         self._validate_cm()
         self._validate_firmwares()
@@ -27,6 +35,9 @@ class CortexM:
         self._state_path = CM_STATE.format(self.core)
 
     def __del__(self):
+        """
+        Destructor method for the Cortex-M class.
+        """
         try:
             self._stop()
         except AttributeError:
@@ -34,10 +45,22 @@ class CortexM:
 
     @property
     def core(self):
+        """
+        Get which Cortex-M core is being used.
+
+        Returns:
+            An integer representing the Cortex-M core.
+        """
         return self._core
 
     @core.setter
     def core(self, core_n):
+        """
+        Set which Cortex-M core is going to be used.
+
+        Args:
+            core_n (int): index of the Cortex-M core.
+        """
         if core_n >= self._num_cores:
             self._core = 0
         else:
@@ -45,6 +68,12 @@ class CortexM:
 
     @property
     def state(self):
+        """
+        Get the current Cortex-M state.
+
+        Returns:
+            A string containing the Cortex-M state.
+        """
         if os.path.isfile(self._state_path):
             with open(self._state_path, 'r') as f:
                 return f.read().strip()
@@ -53,6 +82,12 @@ class CortexM:
 
     @state.setter
     def state(self, new_state):
+        """
+        Set the Cortex-M state.
+
+        Args:
+            new_state (str): the state the Cortex-M is going to be set.
+        """
         if (self.state == "offline" and new_state == CM_START) \
            or (self.state == "running" and new_state == CM_STOP):
             with open(self._state_path, 'w') as f:
@@ -60,6 +95,12 @@ class CortexM:
 
     @property
     def firmware(self):
+        """
+        Get the current Cortex-M firmware.
+
+        Returns:
+            A string containing the Cortex-M firmware.
+        """
         if os.path.isfile(self._firmware_path):
             with open(self._firmware_path, 'r') as f:
                 return f.read().strip()
@@ -68,11 +109,25 @@ class CortexM:
 
     @firmware.setter
     def firmware(self, new_firmware):
+        """
+        Set the Cortex-M firmware.
+
+        Args:
+            new_firmware (str): the firmware that is going
+            to be loaded into the Cortex-M.
+        """
         if os.path.isfile(self._firmware_path):
             with open(self._firmware_path, 'w') as f:
                 f.write(new_firmware)
 
     def run(self, firmware):
+        """
+        Run a given firmware into the Cortex-M.
+
+        Args:
+            firmware (str): the firmware that is going
+            to run into the Cortex-M.
+        """
         if firmware in self.firmwares:
             self._stop()
             self._load(firmware)
@@ -82,6 +137,12 @@ class CortexM:
 
     @staticmethod
     def write(message):
+        """
+        Send a message to a Cortex-M application via virtual serial port.
+
+        Args:
+            message (str): message the is going to be sent.
+        """
         if os.path.exists(CM_TTY):
             with serial.Serial(CM_TTY) as ser:
                 len = ser.write(f"{message}\n".encode())
@@ -92,6 +153,9 @@ class CortexM:
 
     @staticmethod
     def read():
+        """
+        Reads a message from a Cortex-M application via virtual serial port.
+        """
         if os.path.exists(CM_TTY):
             with serial.Serial(CM_TTY, timeout=10) as ser:
                 msg = ser.readline().decode().strip()
@@ -100,6 +164,9 @@ class CortexM:
             print(f"Device not found: {CM_TTY}.")
 
     def _validate_cm(self):
+        """
+        Checks if Cortex-M is available.
+        """
         if not is_cm_enabled():
             sys.exit(f"Error: {CM_REMOTEPROC_DIR} not found.\n"
                      f"Please enable remoteproc driver.\n"
@@ -109,6 +176,10 @@ class CortexM:
                      f" && reboot")
 
     def _validate_firmwares(self):
+        """
+        Checks which files in the firmware directory
+        are valid Cortex-M firmwares.
+        """
         self.firmwares = []
         firmwares_list = list_firmwares()
 
@@ -118,12 +189,27 @@ class CortexM:
                     self.firmwares.append(firmware)
 
     def _start(self):
+        """
+        Start the Cortex-M application and loads
+        the imx_rpms_tty kernel module.
+        """
         self.state = CM_START
         subprocess.run(['modprobe', 'imx_rpmsg_tty'])
 
     def _load(self, firmware):
+        """
+        Set the Cortex-M firmware.
+
+        Args:
+            firmware (str): the firmware that is
+            going to be loaded into the Cortex-M.
+        """
         self.firmware = firmware
 
     def _stop(self):
+        """
+        Stop the Cortex-M application and removes
+        the imx_rpms_tty kernel module.
+        """
         self.state = CM_STOP
         subprocess.run(['modprobe', 'imx_rpmsg_tty', '-r'])
