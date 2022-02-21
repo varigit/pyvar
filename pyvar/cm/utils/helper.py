e0d19d679c9ea76fdd8191eede507e4c70a94fec
Alifer Moraes <alifer.m@variscite.com>
2022-02-20 22:39:01 -0300

    Add documentation for the cm.utils.helper module
    Signed-off-by: Alifer Moraes <alifer.m@variscite.com>

--- pyvar/cm/utils/helper.py
+++ pyvar/cm/utils/helper.py
@@ -15,10 +15,23 @@ from .config import *
 
 
 def is_cm_enabled():
+    """
+    Check if the Cortex-M is enabled.
+
+    Returns:
+        if **enabled**, return **True**
+        if **not**, return **not False**
+    """
     return True if os.listdir(CM_REMOTEPROC_DIR) else False
 
 
 def get_module():
+    """
+    Get the SoM type.
+
+    Returns:
+        A string containing the SoM's type.
+    """
     if not os.path.exists(MACHINE):
         return None
 
@@ -36,14 +49,36 @@ def get_module():
 
 
 def get_cm_cores(module):
+    """
+    Get the number of Cortex-M cores.
+
+    Returns:
+        An integer number of cores.
+    """
     return int(_parse_cm_info(module, 'cm_cores'))
 
 
 def get_cm_dtb(module):
+    """
+    Get the DTB file that enables the Cortex-M.
+
+    Returns:
+        A string with the name of the DTB file.
+    """
     return _parse_cm_info(module, f'cm_dtb_{get_module()}')
 
 
 def _parse_cm_info(module, field):
+    """
+    Get information about the Cortex-M from the variscite-rproc.conf file.
+
+    Args:
+        module (str): the SoM type;
+        field (str): a field of the variscite-rproc.conf file;
+
+    Returns:
+        A string with the required field value.
+    """
     if not module or not os.path.isfile(CM_CONF_FILE):
         return None
 
@@ -56,6 +91,16 @@ def _parse_cm_info(module, field):
 
 
 def _firmware_check(file):
+    """
+    Check if a file is a valid Cortex-M firmware.
+
+    Args:
+        file (str): path to the file;
+
+    Returns:
+        if **is valid**, return **True**
+        if **not**, return **not False**
+    """
     out = subprocess.run(['file', '-b', file], capture_output=True)
     out = out.stdout.decode().strip().lower()
 
@@ -63,6 +108,12 @@ def _firmware_check(file):
 
 
 def list_firmwares():
+    """
+    List all the valid Cortex-M firmwares under the firmware directory.
+
+    Returns:
+        A list containing the valid firmwares
+    """
     firmwares_list = []
 
     if os.path.isdir(CM_FIRMWARE_DIR):
