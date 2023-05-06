101e77783797dfac884f9aeec7c44c9f3f85aa82
Diego Dorta <diego.d@variscite.com>
2023-05-06 14:05:31 -0300

    refactor: add docstring to the image detection examples
    Signed-off-by: Diego Dorta <diego.d@variscite.com>

--- examples/ml/detection/image_detection_armnn.py
+++ examples/ml/detection/image_detection_armnn.py
@@ -1,5 +1,31 @@
-# Copyright 2021 Variscite LTD
+# Copyright 2021-2023 Variscite LTD
 # SPDX-License-Identifier: BSD-3-Clause
+
+"""
+This script performs object detection using the ArmNNInterpreter engine.
+
+It performs the following steps:
+
+1. Retrieves the detection package using a FTP retriever instance.
+2. Loads the labels from the label file.
+3. Creates an ArmNNInterpreter engine instance and a resizer instance.
+4. Resizes the input image to the engine's input size.
+5. Runs inference and gets the detection result.
+6. Creates an overlay instance and draws the output image with the
+   detection result and other information.
+7. Shows the output image using the Multimedia helper.
+
+Example:
+
+To run this script:
+    $ python3 object_detection_armnn.py
+
+Args:
+None.
+
+Returns:
+None.
+"""
 
 from pyvar.ml.engines.armnn import ArmNNInterpreter
 from pyvar.ml.utils.label import Label
